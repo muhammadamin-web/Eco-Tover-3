53a11e891513e6128a59f5594d6046a4a9b7def3
muhammadamin-web <mirboqijonov@gmail.com>
2024-08-25 14:22:18 +0500

    update

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -37,9 +37,10 @@ const Footer = () => {
               <div className="footer_phone">
                 <p className="footer_phone_text">Telefon</p>
                 <a href="tel:1361" className="footer_phone_number">
-                  <BiSolidPhoneCall />
+                  {/* <BiSolidPhoneCall /> */}
                   1361
                 </a>
+                <a href="tel:1104">1104 (24/7)</a>
               </div>
             </div>
             <div className="footer_icons">
