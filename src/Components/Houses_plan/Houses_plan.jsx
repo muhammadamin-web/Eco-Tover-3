19973294785b481c0dd6b44c17f919f5b15ce982
Behruz-developer <88540036+Behruz-developer@users.noreply.github.com>
2024-09-15 14:03:04 +0500

    update

--- src/Components/Houses_plan/Houses_plan.jsx
+++ src/Components/Houses_plan/Houses_plan.jsx
@@ -1,14 +1,12 @@
 import { useState } from "react";
-// import houses1 from "../../assets/images/house1.jpg";
-// import houses11 from "../../assets/images/house11.jpg";
-// import houses12 from "../../assets/images/house12.jpg";
-import houses2 from "../../assets/images/house2.jpg";
-import houses21 from "../../assets/images/house21.jpg";
-import houses22 from "../../assets/images/house22.jpg";
-import houses3 from "../../assets/images/house3.jpg";
-import houses31 from "../../assets/images/house31.jpg";
-import houses32 from "../../assets/images/house32.jpg";
-// import houses4 from "../../assets/images/4-xonali.jpg";
+import room1_1 from "../../assets/images/1-xonali1.jpg";
+import room1_2 from "../../assets/images/1-xonali2.jpg";
+import room2_1 from "../../assets/images/2-xonali1.jpg";
+import room2_2 from "../../assets/images/2-xonali2.jpg";
+import room2_3 from "../../assets/images/2-xonali3.jpg";
+import room3_1 from "../../assets/images/3-xonali1.jpg";
+import room3_2 from "../../assets/images/3-xonali2.jpg";
+
 
 const Houses_plan = () => {
   const [activeNumber, setActiveNumber] = useState("Hammasi");
@@ -24,7 +22,7 @@ const Houses_plan = () => {
           <div className="houses_card_top">
             <h2 className="houses_title">Xonadonlarning o‘ylangan rejalari</h2>
             <div className="houses_card_number">
-              {["Hammasi", "2-xonali", "3-xonali"].map((number) => (
+              {["Hammasi", "1-xonali", "2-xonali", "3-xonali"].map((number) => (
                 <p
                   key={number}
                   className={`houses_number ${
@@ -41,23 +39,22 @@ const Houses_plan = () => {
           {/* Showing corresponding content based on activeNumber */}
           {activeNumber === "Hammasi" && (
             <div className="houses_card_bottom">
-              <img src={houses2} alt="" className="houses_img" />
+              <img src={room2_1} alt="" className="houses_img" />
               <div className="houses_border"></div>
-              <img src={houses21} alt="" className="houses_img" />
+              <img src={room3_1} alt="" className="houses_img" />
               <div className="houses_border"></div>
-              <img src={houses3} alt="" className="houses_img" />
+              <img src={room1_1} alt="" className="houses_img" />
             </div>
           )}
 
-          {/* {activeNumber === "1-xonali" && (
+          {activeNumber === "1-xonali" && (
             <div className="houses_card_bottom1">
-              <img src={houses1} alt="" className="houses_img" />
+              <img src={room1_1} alt="" className="houses_img" />
               <div className="houses_border"></div>
-              <img src={houses11} alt="" className="houses_img" />
-              <div className="houses_border"></div> 
-             <img src={houses12} alt="" className="houses_img" /> 
+              <img src={room1_2} alt="" className="houses_img" />
+
             </div>
-          )} */}
+          )}
 
           {/* {activeNumber === "2-xonali" && (
             <div className="houses_card_bottom2">
@@ -70,20 +67,19 @@ const Houses_plan = () => {
           )} */}
  {activeNumber === "2-xonali" && (
             <div className="houses_card_bottom1">
-              <img src={houses2} alt="" className="houses_img" />
+              <img src={room2_1} alt="" className="houses_img" />
               <div className="houses_border"></div>
-              <img src={houses21} alt="" className="houses_img" />
+              <img src={room2_2} alt="" className="houses_img" />
               <div className="houses_border"></div>
-              <img src={houses22} alt="" className="houses_img" />
+              <img src={room2_3} alt="" className="houses_img" />
             </div>
           )}
           {activeNumber === "3-xonali" && (
             <div className="houses_card_bottom2">
-              <img src={houses3} alt="" className="houses_img" />
+              <img src={room3_1} alt="" className="houses_img" />
               <div className="houses_border"></div>
-              <img src={houses31} alt="" className="houses_img" />
-              <div className="houses_border"></div>
-              <img src={houses32} alt="" className="houses_img" />
+              <img src={room3_2} alt="" className="houses_img" />
+
             </div>
           )}
 
